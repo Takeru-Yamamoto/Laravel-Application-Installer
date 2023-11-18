33a41146dbfd963b5b2af87699fbe3a3e8a248b1
takeru <take.yama0519@gmail.com>
2023-11-19 01:02:49 +0900

    First Published

---
+++ README.md
@@ -0,0 +1,37 @@
+# Laravel Application Installer
+
+## Tested Linux Distributions
+
+* AlmaLinux 9.2
+
+## About
+
+This Installer configure the following Apache settings
+
+* DocumentRoot
+* DirectoryIndex
+* AllowOverride
+
+## Usage
+
+Execute the following command as a user with root privileges.  
+The Installer requires the following arguments
+
+* INSTALL_DIR: The directory where Laravel will be installed
+* OWNER_USER: The user who owns the Laravel directory
+* ACCESS_URL: The URL to access the Laravel application
+* IS_INSTALL_LATEST_LARAVEL: Whether to install the latest version of Laravel
+
+If you want to install the [Takeru-Yamamoto/Laravel-Customized](https://github.com/Takeru-Yamamoto/Laravel-Customized.git), set the argument IS_INSTALL_LATEST_LARAVEL to 1.
+
+Please replace the respective argument parts and execute.
+
+```
+curl -s https://raw.githubusercontent.com/Takeru-Yamamoto/Laravel-Application-Installer/script.bash | bash -s "${INSTALL_DIR}" "${OWNER_USER}" "${ACCESS_URL}" "${IS_INSTALL_LARAVEL_CUSTOMIZED}"
+```
+
+As soon as the building is complete, run the Laravel migration.
+
+```
+php artisan migrate --seed
+```
---
+++ script.bash
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+# 変数の受け取り
+
+## Laravel をインストールするディレクトリを受け取り
+readonly INSTALL_DIR="$1"
+if [[ -z "${INSTALL_DIR}" ]]; then
+    echo "Please enter the directory name to install Laravel."
+    exit 1
+fi
+
+## Laravel をインストールするディレクトリの所有権を持つユーザーを受け取り
+readonly OWNER_USER="$2"
+if [[ -z "${OWNER_USER}" ]]; then
+    echo "Please enter the user who owns the directory to install Laravel."
+    exit 1
+fi
+
+## Laravel にアクセスする為の URL を受け取り
+readonly ACCESS_URL="$3"
+if [[ -z "${ACCESS_URL}" ]]; then
+    echo "Please enter the URL to access Laravel."
+    exit 1
+fi
+
+## Laravel-Customizedをインストールするかどうかを受け取り
+readonly IS_INSTALL_LATEST_LARAVEL=${4-0}
+
+# コメントの表示
+echo ""
+echo "--------------------------------------------------"
+echo "Start Installing Laravel Application."
+echo "--------------------------------------------------"
+echo ""
+
+## ダウンロードするディレクトリが存在しない場合は作成
+if [ ! -d "/var/www/html/${INSTALL_DIR}" ]; then
+    mkdir -p "/var/www/html/${INSTALL_DIR}"
+fi
+
+## ダウンロードするディレクトリに移動
+cd "/var/www/html/${INSTALL_DIR}"
+
+## Laravel のダウンロード
+## IS_INSTALL_LATEST_LARAVEL が 0 の場合は Laravel の最新版をインストール
+## IS_INSTALL_LATEST_LARAVEL が 1 の場合は Laravel-Customized をインストール
+if [[ "${IS_INSTALL_LATEST_LARAVEL}" -eq 0 ]]; then
+    composer create-project --prefer-dist laravel/laravel .
+else
+    git clone https://github.com/Takeru-Yamamoto/Laravel-Customized.git .
+fi
+
+# Laravel の初期設定
+
+## Laravel のディレクトリに移動
+cd "/var/www/html/${INSTALL_DIR}"
+
+## .env.exampleのAPP_URLを変更
+sed -i -e "s/APP_URL=http:\/\/localhost/APP_URL=http:\/\/${ACCESS_URL}/g" .env.example
+
+## .env ファイルをコピー
+cp .env.example .env
+
+## vendor 配下のファイルをComposerでインストール
+composer update
+
+## Composerのオートロードを更新
+composer dump-autoload
+
+## APP_KEY を生成
+php artisan key:generate
+
+## storage 配下のファイルのシンボリックリンクを作成
+php artisan storage:link
+
+## Laravel のディレクトリで npm を実行
+npm install && npm run build
+
+# Laravel のディレクトリの所有権とパーミッションを変更
+
+## Laravel のディレクトリの所有権を変更
+chown -R "${OWNER_USER}:${OWNER_USER}" "/var/www/html/${INSTALL_DIR}"
+
+## Laravel のディレクトリのパーミッションを変更
+chmod -R 755 "/var/www/html/${INSTALL_DIR}"
+
+## Storage ディレクトリのパーミッションを変更
+chmod -R 777 "/var/www/html/${INSTALL_DIR}/storage"
+
+# Apacheの設定変更
+
+## httpd.conf のバックアップ
+cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.bak
+
+## httpd.conf の編集
+sed -i -e "s/DocumentRoot \"\/var\/www\/html\"/DocumentRoot \"\/var\/www\/html\/${INSTALL_DIR}\/public\"/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/<Directory \"\/var\/www\/\">/<Directory \"\/var\/www\/html\/${INSTALL_DIR}\/public\">/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/AllowOverride None/AllowOverride All/g" /etc/httpd/conf/httpd.conf
+sed -i -e "s/DirectoryIndex index.html/DirectoryIndex index.php index.html/g" /etc/httpd/conf/httpd.conf
+
+## Apache の再起動
+systemctl restart httpd
+
+# コメントの表示
+echo ""
+echo "--------------------------------------------------"
+echo "Complete Installing Laravel Application."
+echo "Please make the appropriate edits and perform the migration ans seeding."
+echo "--------------------------------------------------"
+echo ""
